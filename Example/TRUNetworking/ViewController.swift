7e01ae1d149d225975f6ee4e6ff56822d364263b
黎炜烨 <liweiye.1997@bytedance.com>
2020-01-20 15:30:14 +0800

    Initial commit
    Change-Id: Ib07735568b9b578970322499192e15a7416dcbd5

---
+++ Example/TRUNetworking/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TRUNetworking
+//
+//  Created by liweiye1997@gmail.com on 01/20/2020.
+//  Copyright (c) 2020 liweiye1997@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
